f862294ff967d4ee8dba99fe30b0294fb319f673
unknown <1392096101@qq.com>
2017-06-21 11:20:00 +0800

    Signed-off-by: unknown <1392096101@qq.com>

---
+++ app/src/main/java/com/example/yls/webviewtest/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.yls.webviewtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        mWebView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if(newProgress == 100){
+                    Toast.makeText(MainActivity.this,"网页加载完了 可以显示评论区了",Toast.LENGTH_LONG).show();
+                }
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+        mWebView.loadUrl("http://www.baidu.com");
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            if(mWebView.canGoBack()){
+                mWebView.goBack();
+                return true;
+            }
+        }
+
+        return super.onKeyDown(keyCode,event);
+    }
+}
